0740554e2d515e6d96cbd22a0d5d07573c43f2da
Gi-Hwang Kim <hangole1999@gmail.com>
2019-08-09 21:28:24 +0900

    [Auto] Fix many

--- src/i18n/en.js
+++
@@ -1,5 +0,0 @@
-
-export default {
-  language: 'Language',
-  footer: 'Copyright&copy; {date}. Hangle. All rights reserved.'
-};
--- src/i18n/ko.js
+++
@@ -1,5 +0,0 @@
-
-export default {
-  language: '언어',
-  footer: 'Copyright&copy; {date}. Hangle. All rights reserved.'
-};
